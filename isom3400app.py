7a0c9ac562c994ac66f01303b21f24ea8215f291
leoli02468 <leoli02468@gmail.com>
2026-04-24 12:31:48 +0800

    Update isom3400app.py

--- isom3400app.py
+++ isom3400app.py
@@ -2,3 +2,15 @@ import streamlit as st
 
 st.title("Hello")
 st.write("isom3400")
+
+st.write("**Bold Text** and *Italic Text*")
+
+age = st.number_input("Enter your age:",
+                      min_value=0,
+                      max_value=120,
+                      value=25)
+st.write(f"Your age is {age}")
+
+option = st.selectbox("Choose your favorite color:",
+                      ["Red", "Blue", "Green"])
+st.write(f"You selected: {option}")
