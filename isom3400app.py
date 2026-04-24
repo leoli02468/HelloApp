d8b66043fbff02c7ffac18bc4704cbdbb5eb2fd0
leoli02468 <leoli02468@gmail.com>
2026-04-24 12:53:51 +0800

    Update isom3400app.py

--- isom3400app.py
+++ isom3400app.py
@@ -9,7 +9,7 @@ st.write("Please enter the monthly sales target and the region.")
 age = st.number_input("Enter Monthly Sales Target (in USD): ",
                      min_value=0,
                      max_value=50000,
-                     value=50000))
+                     value=50000)
 
                       
 color = st.selectbox("Select Region:",
