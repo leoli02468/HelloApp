da5a0453041155243807f0bff3d153819af5c9cf
leoli02468 <leoli02468@gmail.com>
2026-04-24 13:00:57 +0800

    Update isom3400app.py

--- isom3400app.py
+++ isom3400app.py
@@ -6,14 +6,15 @@ st.header("Manager Input Section")
 st.write("Please enter the monthly sales target and the region.")
          
 
-age = st.number_input("Enter Monthly Sales Target (in USD): ",
+monthly_sales = st.number_input("Enter Monthly Sales Target (in USD): ",
                      min_value=0,
                      max_value=50000,
                      value=50000)
-
+st.write(f"The sales target entered: ${monthly_sales}")
                       
-color = st.selectbox("Select Region:",
+region = st.selectbox("Select Region:",
                      ["North", "South", "East", "West"])
+st.write(f"The selected region: {region}")
 
 if st.button("Submit"):
     st.success("Dashboard updated successfully!")
