ef6b70d10c264e6d4a184673eeb874067bcf9482
leoli02468 <leoli02468@gmail.com>
2026-04-24 12:53:12 +0800

    Update isom3400app.py

--- isom3400app.py
+++ isom3400app.py
@@ -3,12 +3,13 @@ import streamlit as st
 st.title("Retail Business Dashboard")
 st.header("Manager Input Section")
 
-st.write("Please enter the monthly sales target and the region.",
-         min_value=0,
-         max_value=50000,
-         value=50000)
+st.write("Please enter the monthly sales target and the region.")
+         
 
-age = st.number_input("Enter Monthly Sales Target (in USD): ")
+age = st.number_input("Enter Monthly Sales Target (in USD): ",
+                     min_value=0,
+                     max_value=50000,
+                     value=50000))
 
                       
 color = st.selectbox("Select Region:",
