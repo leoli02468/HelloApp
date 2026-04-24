b476e5f8e5015026635de4793c6d2e478e927268
leoli02468 <leoli02468@gmail.com>
2026-04-24 12:50:37 +0800

    Update isom3400app.py

--- isom3400app.py
+++ isom3400app.py
@@ -6,7 +6,7 @@ st.header("Manager Input Section")
 st.write("Please enter the monthly sales target and the region.",
          min_value=0,
          max_value=50000,
-         value=50000))
+         value=50000)
 
 age = st.number_input("Enter Monthly Sales Target (in USD): ")
 
